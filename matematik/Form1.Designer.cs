c8836fd886e74b3a3e4f0deeea72f4c4d55efccb
rumeysayrtsvr <146195503+rumeysayrtsvr@users.noreply.github.com>
2025-04-22 13:27:48 +0300

    Add files via upload

---
+++ matematik/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace matematik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelCevapSayisi = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(9, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SEVİYE 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(95, 20);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SEVİYE 2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(186, 20);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SEVİYE 3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(271, 20);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 24);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "SEVİYE 4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(364, 20);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 24);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "SEVİYE 5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(10, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 28);
+            this.label1.TabIndex = 5;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 18);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 29);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.buttonNext);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(95, 69);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(204, 263);
+            this.panel1.TabIndex = 15;
+            this.panel1.Visible = false;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonNext.Location = new System.Drawing.Point(136, 220);
+            this.buttonNext.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(56, 31);
+            this.buttonNext.TabIndex = 16;
+            this.buttonNext.Text = "İleri";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(117, 173);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(42, 29);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(117, 135);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(42, 29);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 95);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(42, 29);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 57);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 29);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(10, 173);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 28);
+            this.label5.TabIndex = 9;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(10, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 28);
+            this.label4.TabIndex = 8;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(10, 95);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 28);
+            this.label3.TabIndex = 7;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(10, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 28);
+            this.label2.TabIndex = 6;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelCevapSayisi
+            // 
+            this.labelCevapSayisi.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelCevapSayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelCevapSayisi.Location = new System.Drawing.Point(92, 345);
+            this.labelCevapSayisi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelCevapSayisi.Name = "labelCevapSayisi";
+            this.labelCevapSayisi.Size = new System.Drawing.Size(116, 21);
+            this.labelCevapSayisi.TabIndex = 16;
+            this.labelCevapSayisi.Text = "Doğru Sayısı: 0";
+            this.labelCevapSayisi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::matematik.Properties.Resources.yaziTahtasi;
+            this.ClientSize = new System.Drawing.Size(650, 424);
+            this.Controls.Add(this.labelCevapSayisi);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Label labelCevapSayisi;
+    }
+}
+
---
+++ matematik/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace matematik
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        private Label[] soruLabels;
+        private int LevelToplamSoru = 20; // Toplam soru sayısını 20 olarak ayarladık
+        private int mevcutLevel = 1; // Mevcut seviye değişkeni
+        private int dogruCevapSayisi = 0; // Doğru cevap sayısı
+        private int toplamCevapSayisi = 0; // Toplam cevaplanan soru sayısı
+        private int currentQuestionIndex = 0; // Şu anki soru indeksi
+
+        public Form1()
+        {
+            InitializeComponent();
+            soruLabels = new Label[] { label1, label2, label3, label4, label5 };
+            buttonNext.Click += new EventHandler(buttonNext_Click); // İleri butonuna tıklanma olayı ekle
+            LoadQuestions(); // Soruları yükle
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            ClearPreviousAnswers(); // Oyun başladığında önceki cevapları temizle
+            LoadQuestions(); // Yeni soruları yükle
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (toplamCevapSayisi < LevelToplamSoru) // Eğer toplam soru sayısına ulaşmadıysak
+            {
+                CheckCurrentAnswers(); // Geçerli cevapları kontrol et
+                ClearPreviousAnswers(); // Önceki cevapları temizle
+                currentQuestionIndex += 5; // Her seferinde sonraki 5 soruya geç
+                toplamCevapSayisi += 5; // Cevaplanan soru sayısını artır
+
+                if (toplamCevapSayisi < LevelToplamSoru) // Eğer henüz 20. soraya ulaşmadıysa
+                {
+                    LoadQuestions(); // Yeni soruları yükle
+                }
+                else // 20. soraya ulaşıldıysa
+                {
+                    CheckAllAnswers(); // Tüm cevapları kontrol et
+                }
+            }
+        }
+
+        private void ClearPreviousAnswers()
+        {
+            foreach (Label lbl in soruLabels)
+            {
+                lbl.Tag = null; // Önceki cevapları temizle
+                lbl.Text = ""; // Label metnini temizle
+            }
+
+            // Tüm TextBox'ları temizle
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+
+        private string GetRandomOperationLevel1()
+        {
+            string[] operations = { "+", "-" }; // Sadece toplama ve çıkarma
+            return operations[random.Next(operations.Length)];
+        }
+
+        private void LoadQuestions()
+        {
+            for (int i = 0; i < soruLabels.Length; i++)
+            {
+                if (currentQuestionIndex + i < LevelToplamSoru) // Sadece mevcut soru indeksine göre yükle
+                {
+                    int num1 = random.Next(1, 21);
+                    int num2 = random.Next(1, 21);
+                    string operation = GetRandomOperationLevel1(); // Sadece toplama ve çıkarma için özel fonksiyon
+
+                    // Soru oluştur ve label'a yaz
+                    string soru = $"{num1} {operation} {num2} = ?";
+                    soruLabels[i].Text = soru;
+
+                    // Doğru cevabı sakla
+                    soruLabels[i].Tag = Hesapla(num1, num2, operation); // Doğru cevabı etiketin Tag özelliğine kaydet
+                }
+            }
+        }
+
+        private void CheckCurrentAnswers()
+        {
+            for (int i = 0; i < soruLabels.Length; i++)
+            {
+                if (soruLabels[i].Tag != null) // Eğer label'da bir cevap varsa
+                {
+                    int dogruCevap = (int)soruLabels[i].Tag; // Doğru cevabı al
+                    int kullaniciCevabi;
+                    string userInput = GetUserAnswer(i); // Kullanıcı cevabını al
+
+                    // Kullanıcı cevaplarını kontrol et
+                    if (int.TryParse(userInput, out kullaniciCevabi))
+                    {
+                        if (kullaniciCevabi == dogruCevap)
+                        {
+                            dogruCevapSayisi++; // Doğru cevap sayısını artır
+                        }
+                    }
+                }
+            }
+            labelCevapSayisi.Text = $"Doğru Cevap Sayısı: {dogruCevapSayisi}"; // Doğru cevap sayısını göster
+        }
+
+        private void CheckAllAnswers()
+        {
+            MessageBox.Show($"Tüm sorular tamamlandı! Doğru cevap sayınız: {dogruCevapSayisi}"); // Tüm sorular tamamlandığında mesaj göster
+            // Burada sonuçları gösterebilirsiniz.
+        }
+
+        private int Hesapla(int num1, int num2, string operation)
+        {
+            if (operation == "+")
+            {
+                return num1 + num2;
+            }
+            else if (operation == "-")
+            {
+                return num1 - num2;
+            }
+            else
+            {
+                return 0; // Geçersiz işlem durumunda 0 döndür
+            }
+        }
+
+        private string GetUserAnswer(int index)
+        {
+            switch (index)
+            {
+                case 0: return textBox1.Text; // label1 için cevap
+                case 1: return textBox2.Text; // label2 için cevap
+                case 2: return textBox3.Text; // label3 için cevap
+                case 3: return textBox4.Text; // label4 için cevap
+                case 4: return textBox5.Text; // label5 için cevap
+                default: return ""; // Geçersiz indeks
+            }
+        }
+    }
+}
